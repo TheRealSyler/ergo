4110f714d0975e811dd5d553703093bc7309c54f
Leonard Grosoli <hileo77@gmail.com>
2021-09-14 12:43:39 +0200

    update loader.

--- src/ui/loaderUI.tsx
+++ src/ui/loaderUI.tsx
@@ -1,11 +1,22 @@
 import { h } from 'dom-chef'
+import { LoadingManager } from 'three'
 import './loaderUI.sass'
 import { MAIN_UI_ELEMENT } from './ui'
 
-export function LoaderUI() {
+export function LoaderUI(manager?: LoadingManager, info?: string) {
   MAIN_UI_ELEMENT.textContent = ''
 
+  const progressEL = <div className="loader-progress">0</div>
+  if (manager) {
+    manager.onProgress = (url, number, total) => {
+      progressEL.textContent = `${number} / ${total} - ${url}`
+    }
+  }
+
   MAIN_UI_ELEMENT.appendChild(<div className="loader">
+    <div className="loader-info">{info}</div>
     <div className="loader-spinner"></div>
+    {manager && progressEL}
   </div>)
+
 }
