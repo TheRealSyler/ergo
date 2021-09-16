0d795aced0a7f4c3141c8b736240f230fe220afd
Leonard Grosoli <hileo77@gmail.com>
2021-09-16 22:38:11 +0200

    add bar component

---
+++ src/ui/barComponent.tsx
@@ -0,0 +1,26 @@
+import { h } from 'dom-chef'
+import { toFixedIfNotZero } from '../utils'
+import './barComponent.sass'
+
+export class BarComponent {
+  private textEl = <div className="bar-text"></div>
+  private barEl = <div className="bar-bar">{this.textEl}</div>
+  private mainEl = <div className="bar-main">{this.barEl}</div>
+  constructor(type: 'stamina' | 'health') {
+    if (type === 'health') {
+      this.barEl.style.background = 'red'
+    } else {
+      this.barEl.style.background = 'green'
+    }
+
+  }
+
+  getEl() {
+    return this.mainEl
+  }
+
+  set(current: number, max: number) {
+    this.textEl.textContent = toFixedIfNotZero(current, 1)
+    this.barEl.style.width = `${(current / max) * 100}%`
+  }
+}
