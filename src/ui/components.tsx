85800c27ed1e2221ba745f9ca0bed2747d7293dd
Leonard Grosoli <hileo77@gmail.com>
2022-02-22 03:00:10 +0100

    update components css and add LevelEl

--- src/ui/components.tsx
+++ src/ui/components.tsx
@@ -2,9 +2,12 @@ import { h } from 'dom-chef'
 import './components.sass'
 
 export function MoneyEl(amount: string | number) {
-  return <span className="money">{amount}</span>
+  return <span className="component-money">{amount}</span>
+}
+export function LevelEl(amount: string | number) {
+  return <span className="component-level">{amount}</span>
 }
 
 export function StatEl(amount: string | number, positive: boolean) {
-  return <span className={`stat ${positive ? 'stat-positive' : 'stat-negative'}`}>{amount}</span>
+  return <span className={`stat ${positive ? 'component-stat-positive' : 'component-stat-negative'}`}>{amount}</span>
 }
